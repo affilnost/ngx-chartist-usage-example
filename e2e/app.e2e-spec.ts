59b9fbc59a15b5f261c9d421c8394802be63c321
Anastasiia Antonova <antonova64@gmail.com>
2018-09-26 21:50:53 +0200

    updated ngx-chartist nmp package, small fixes

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ngx-chartist-example-usage App', () => {
+describe('ngx-chartist-usage-example App', () => {
   let page: AppPage;
 
   beforeEach(() => {
